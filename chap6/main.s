dd13c6db2da80a348066594ff531c8fac542376c
ameyakakade <ameyakakade@gmail.com>
2026-05-16 18:33:23 +0530

    Wrote asm program that calls a function to convert string into upper case. It takes in arguments and returns a value

--- chap6/main.s
+++ chap6/main.s
@@ -1,5 +1,5 @@
 // this function converts a string into uppercase by calling a function
-    .global start_
+    .global _start
 
 _start:
     // store add of input string in x0
@@ -13,12 +13,17 @@ _start:
 
     // printing the string
     MOV  X2, X0 // the function returns length of the string
-    SUBS X5, X5, #1
-    MOV  X16, #0x4
     MOV  X0,  #1
+    ADRP X1, outstr@PAGE
+    ADD  X1, X1, outstr@PAGEOFf
+    MOV  X16, #0x4
     SVC #0x80
 
     // exit
     MOV X16, #1
     MOV X0,  #0
     SVC #0x80
+
+    .data
+instr:  .asciz "This is a null terminated string. \nThis assembly program calls a function in another assembly program. \nI might be able to call the uppercase function from a high level language like C."
+outstr: .fill 255, 1, 0 // 255 bytes filled with zero
---
+++ chap6/upper.s
@@ -0,0 +1,37 @@
+    // this file contains the toupper function that will be called
+    .global toupper // allow other files to call this routine
+
+toupper:
+    // X0 has address of input string
+    // X1 has address of output string
+    // function returns length of the string
+
+    // return to instr in lr
+    // store lr in stack
+    STR LR, [SP, #-16]!
+    MOV X3, #-1 // we dont need to print last char because it is null
+
+loop:   
+    // W2 holds current character
+    // X3 holds number of chars
+    LDRB W2, [X0], #1 // loading char and incrementing X0 by 1
+
+    CMP W2, 'a'
+    B.LT jump
+    CMP W2, 'z'
+    B.GT jump
+
+    // if we get here that means it is a lowercase char
+    // so we convert it to uppercase
+    SUB W2, W2, #('a'-'A')
+
+jump:
+    STRB W2, [X1], #1 // storing char and incrementing X1 by 1
+    ADD X3, X3, #1
+    CMP W2, #0 // if its null do not loop
+    B.NE loop
+
+    // load lr from stack
+    LDR LR, [SP], #16
+    MOV X0, X3 // store length in x5
+    RET
